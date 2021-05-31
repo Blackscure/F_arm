75b906ba76472c89115b91a570aef57800bd91a5
Blackscure <wekesabuyahi@gmail.com>
2021-05-31 14:19:26 +0300

    my location padding added

--- src/components/Home.js
+++ src/components/Home.js
@@ -65,21 +65,23 @@ export class Home extends Component {
                         </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
+                    
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-list-4" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbar-list-4">
+                    <div class="collapse navbar-collapse" id="navbar-list-4" id="profile">
                     <ul class="navbar-nav">
                     <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="basic-nav-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg" width="40" height="40" class="rounded-circle"/>
+                    <img src="https://images.unsplash.com/photo-1509506489701-dfe23b067808?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1668&q=80" width="40" height="40" class="rounded-circle"/>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="basic-nav-dropdown">
-                        <a class="dropdown-item" href="#">Dashboard</a>
-                        <a class="dropdown-item" href="#">Edit Profile</a>
-                        <a class="dropdown-item" href="#">Log Out</a>
+                        <a class="dropdown-item" href="#">My Profile</a>
+                        <a class="dropdown-item" href="#">Account</a>
+                        <a class="dropdown-item" href="#">Sign Out</a>
+                        <button onClick={handleLogout}>Sign Out</button>
                     </div>
-                    <button onClick={handleLogout}>Temporary Logout btn</button>
+                     
                     </li>
                     </ul>
                     </div>
@@ -111,7 +113,6 @@ export class Home extends Component {
             </UncontrolledButtonDropdown>
      </div>
     </nav>
-       
     </div>
 </div>
 
@@ -233,7 +234,6 @@ export class Home extends Component {
 </div>
 
 <div id='right' className={rightOpen} >
-   
     <h5>Calender</h5>
     
     <form className="form-inline">
